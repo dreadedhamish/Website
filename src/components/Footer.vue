3bf9121040ed3a65ebcb835c44d36aea70f5ea44
P <72272744+paultheg@users.noreply.github.com>
2021-07-30 02:38:46 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,6 @@
 <template>
 	<div>
-		<!-- Section 1 -->
-		<section class="bg-white footer-wrapper">
+		<section class="bg-white">
 			<div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
 				<div class="flex justify-center mt-8 space-x-6">
 					<a href="https://discord.gg/sA8AdfF7ac" class="text-gray-400 hover:text-gray-500">
@@ -37,11 +36,3 @@
 	</div>
 </template>
 
-<style scoped>
-	.footer-wrapper {
-		background-image: url(https://ipfs.io/ipfs/Qma11mYE8TWQGntt2bZbX9ktJE7D7ACprLP9QcntxGSmjW/);
-		background-repeat: no-repeat;
-		background-size: cover;
-		background-position: 75% 1%, 0 0;
-	}
-</style>
