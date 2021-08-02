c32cca3328ce345274c2c585d9fb1209157fd03e
BeSoEasy <besoeasy@users.noreply.github.com>
2021-08-02 10:40:29 +0530

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,6 +12,7 @@
 					<nav class="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200 justify-center">
 						<router-link to="/token" class="uppercase mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">Token</router-link>
 						<router-link to="/tvl" class="uppercase mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">TVL</router-link>
+						<router-link to="/faq" class="uppercase mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">FAQ</router-link>
 						<router-link to="/roadmap" class="uppercase mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">Roadmap</router-link>
 						<router-link to="/contact" class="uppercase mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">Contact</router-link>
 					</nav>
